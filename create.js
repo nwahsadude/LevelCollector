4a02b1341302619e5a88bfdd0f4134e5bbd2292e
nwahsadude <nwahsadude@gmail.com>
2020-04-04 09:18:55 -0500

    Only let them submit one level at a time.

--- create.js
+++ create.js
@@ -1,90 +1,89 @@
-'use strict';
+"use strict";
 
-const uuid = require('uuid');
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const uuid = require("uuid");
+const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.create = (event, context, callback) => {
-    const timestamp = new Date().getTime();
-    let queryString = event.queryStringParameters;
+  const timestamp = new Date().getTime();
+  let queryString = event.queryStringParameters;
 
+  if (queryString.levelCode.length !== 11) {
+    const response = {
+      statusCode: 200,
+      body: `@${queryString.userName} this level code is not valid`
+    };
+    callback(null, response);
+  }
 
-    if(queryString.levelCode.length !== 11){
-        const response = {
-            statusCode: 200,
-            body: `@${queryString.userName} this level code is not valid`
-        };
-        callback(null, response);
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      levelCode: queryString.levelCode,
+      userName: queryString.userName,
+      createdAt: timestamp,
+      cleared: false,
+      skipped: false,
+      completed: false
+    }
+  };
+
+  dynamoDb.scan(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { "Content-Type": "text/plain" },
+        body: "Couldn't fetch the todos."
+      });
+      return;
     }
 
-    const params = {
-        TableName: process.env.DYNAMODB_TABLE,
-        Item: {
-            id: uuid.v1(),
-            levelCode: queryString.levelCode,
-            userName: queryString.userName,
-            createdAt: timestamp,
-            cleared: false,
-            skipped: false,
-            completed: false
-        },
-    };
-
-    dynamoDb.scan(params, (error, result) => {
-        // handle potential errors
-        if (error) {
-            console.error(error);
-            callback(null, {
-                statusCode: error.statusCode || 501,
-                headers: { "Content-Type": "text/plain" },
-                body: "Couldn't fetch the todos."
-            });
-            return;
-        }
-
-        let data = result.Items.filter(value => {
-            return !value.skipped && !value.cleared && queryString.levelCode === value.levelCode
-        });
-
-
-        if(data.length === 0){
-
-            // write the todo to the database
-            dynamoDb.put(params, (error) => {
-                // handle potential errors
-                if (error) {
-                    console.error(error);
-                    callback(null, {
-                        statusCode: error.statusCode || 501,
-                        headers: { 'Content-Type': 'text/plain' },
-                        body: 'Couldn\'t create the todo item.'
-                    });
-                    return;
-                }
-
-                // create a response
-                const response = {
-                    statusCode: 200,
-                    body: `@${queryString.userName} your level has been submitted!`,
-                };
-                callback(null, response);
-            });
-        } else {
-            const response = {
-                headers: {
-                    "Access-Control-Allow-Origin": "*"
-                },
-                statusCode: 200,
-                body: `@${queryString.userName} this level has already been submitted`
-            };
-            callback(null, response);
-        }
-
-
-
+    let data = result.Items.filter(value => {
+      return (
+        !value.skipped &&
+        !value.cleared &&
+        queryString.levelCode === value.levelCode
+      );
     });
 
+    let usersLevels = result.Items.filter(value => {
+      return (
+        !value.skipped &&
+        !value.cleared &&
+        value.userName === queryString.userName
+      );
+    });
 
-
+    if (data.length === 0 && usersLevels.length === 0) {
+      dynamoDb.put(params, error => {
+        const response = {
+          statusCode: 200,
+          body: `@${queryString.userName} your level has been submitted!`
+        };
+        callback(null, response);
+      });
+    } else if (usersLevels.length === 0 && data.length > 0) {
+      const response = {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        },
+        statusCode: 200,
+        body: `@${queryString.userName} this level has already been submitted`
+      };
+      callback(null, response);
+    } else if (usersLevels.length > 0 && data.length === 0) {
+      const response = {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        },
+        statusCode: 200,
+        body: `@${queryString.userName} please wait until your level has been played before submitting another.`
+      };
+      callback(null, response);
+    }
+  });
 };
